c8e394031894439fc3d41d2067d42fb8255f40bb
Mark Boleigha <donhoenix@gmail.com>
2020-09-30 10:36:53 +0100

    create project

---
+++ streaming/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  streaming
+//
+//  Created by Sprinthub on 30/09/2020.
+//  Copyright © 2020 quacklabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
